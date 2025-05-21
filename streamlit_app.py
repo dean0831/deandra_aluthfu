1b45d1c12af4eab2e9d43899badb20e89741b3c4
dean0831 <deandraaluthfu@gmail.com>
2025-05-21 09:15:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("♥️ hai oll")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "bermain bersamaa."
 )
